80473acfc69588b607b8eb5accf679d5f093e5ba
classroom <classroom@M306-9.brdeng.ac>
2025-11-06 15:30:23 +0200

    added javadoc file

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,37 +1,44 @@
-import java.util.ArrayList;
+public class SharedData 
+{
+	private int [] array;
+	private boolean [] winArray;
+	private boolean flag;
+	private final int b;
 
-public class SharedData {
-    private ArrayList<Integer> array;
-    private boolean[] winArray;
-    private boolean flag;
-    private final int b;
+	
+	public SharedData(int[] array, int b) {
+		
+		this.array = array;
+		this.b = b;
+	}
 
-    public SharedData(ArrayList<Integer> array, int b) {
-        this.array = array;
-        this.b = b;
-    }
+	public boolean[] getWinArray() 
+	{
+		return winArray;
+	}
 
-    public boolean[] getWinArray() {
-        return winArray;
-    }
+	public void setWinArray(boolean [] winArray) 
+	{
+		this.winArray = winArray;
+	}
 
-    public void setWinArray(boolean[] winArray) {
-        this.winArray = winArray;
-    }
+	public int[] getArray() 
+	{
+		return array;
+	}
 
-    public ArrayList<Integer> getArray() {
-        return array;
-    }
+	public int getB() 
+	{
+		return b;
+	}
 
-    public int getB() {
-        return b;
-    }
+	public boolean getFlag() 
+	{
+		return flag;
+	}
 
-    public boolean getFlag() {
-        return flag;
-    }
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
 
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
 }
